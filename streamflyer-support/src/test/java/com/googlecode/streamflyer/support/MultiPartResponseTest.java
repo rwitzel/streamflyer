56e9c5f7d21e8fd44057b4f171bb7660c2d2e943
rodrigo witzel <rodrigo.witzel@gmx.de>
2015-03-23 12:12:56 +0100

    Issue 3:	Closing the underlying writer should be optional
      test rewritten as it appears in the discussion group

--- streamflyer-support/src/test/java/com/googlecode/streamflyer/support/MultiPartResponseTest.java
+++ streamflyer-support/src/test/java/com/googlecode/streamflyer/support/MultiPartResponseTest.java
@@ -24,23 +24,23 @@ public class MultiPartResponseTest {
     public void testWritingMultipartResponse() throws Exception {
         
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
         OutputStreamWriter originalWriter = new OutputStreamWriter(outputStream, "UTF-8");
-
-        ModifyingWriter modifyingWriterPart1 =
-            new ModifyingWriterFactory().createRegexModifyingWriter(originalWriter, "abcd", "1234");
-
-        ModifyingWriter modifyingWriterPart2 =
-                new ModifyingWriterFactory().createRegexModifyingWriter(originalWriter, "abcd", "1234");
 
         originalWriter.flush(); // make sure everything is written
 
+        // write part 1
+        ModifyingWriter modifyingWriterPart1 =
+                new ModifyingWriterFactory().createRegexModifyingWriter(originalWriter, "abcd", "1234");
         modifyingWriterPart1.write("mod ");
         modifyingWriterPart1.close(false);
 
+        // write to the underlying writer
         originalWriter.write("orig ");
         originalWriter.flush();
-        
+
+        // write part 2
+        ModifyingWriter modifyingWriterPart2 =
+                new ModifyingWriterFactory().createRegexModifyingWriter(originalWriter, "abcd", "1234");
         modifyingWriterPart2.write("mod ");
         modifyingWriterPart2.close(false);
 
