370446193686f0953c413165066b751d0ee248ae
Rodrigo Witzel <rwitzel75@googlemail.com>
2015-03-24 18:00:18 +0100

    delete: everything because this is the branch for static webfiles

--- streamflyer-core/generate-dependency-info.sh
+++
@@ -1,7 +0,0 @@
-
-rm -f generated-runtime-deps-list.txt
-rm -f generated-runtime-deps-tree.txt
-
-mvn dependency:list -DincludeScope=runtime -DoutputFile=generated-runtime-deps-list.txt -Dsort=true
-mvn dependency:tree -Dscope=runtime -DoutputFile=generated-runtime-deps-tree.txt
- 
